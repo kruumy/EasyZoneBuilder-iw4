86d3676b7ac5f09ce3c26325a30da0ff83c894a9
kruumy <pauls@JOSH-PC>
2023-03-15 23:43:55 -0400

    added preprocesser statement

--- EasyZoneBuilder.GUI/App.xaml.cs
+++ EasyZoneBuilder.GUI/App.xaml.cs
@@ -11,10 +11,12 @@ namespace EasyZoneBuilder.GUI
     {
         private void Application_DispatcherUnhandledException( object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e )
         {
+#if RELEASE
             string ErrorDumpPath = Path.Combine(Environment.CurrentDirectory, $"error_{DateTime.Now:yyyy-dd-M--HH-mm-ss}.txt");
             File.WriteAllText(ErrorDumpPath, e.Exception.ToString());
             MessageBox.Show($"Error:\n\n{e.Exception.Message}\n\nWrote full error details to:\n\n\"{ErrorDumpPath}\"", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             e.Handled = true;
+#endif
         }
     }
 }
